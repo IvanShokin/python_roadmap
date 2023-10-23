d5222fc8a6a81a91d30122e5ed91a9c9a59076f0
Nerzhul <sinokbati@gmail.com>
2023-10-23 22:22:41 +0300

    init

--- 001_datatype.py
+++ 001_datatype.py
@@ -1,11 +1,14 @@
-x = 1  # int
-x2 = 123
-y = 'asdads'  # str
+x = 11  # int
 z = 1.2  # float
 q = True  # bool
+y = 'asdads'  # str
 
-print(x == x2)
-print(x >= x2)
-print(x <= x2)
-print(x >= x2)
+list_1 = ['Надежда', 'Иван', 'Вася', 123, True, 1.2, [12, 23, 44, [23, 34]]]
+tuple_1 = ('Надежда', 'Иван', 'Вася', 123, True, 1.2, [12, 23, 44, [23, 34]])
 
+
+d_1 = {
+    list_1: '24323'
+}
+
+print(d_1)
